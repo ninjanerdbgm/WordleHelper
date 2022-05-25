a626f971bca39cecc5ab3639b4ff368bc133ac23
matt <ninjanerd2781@gmail.com>
2022-05-25 10:40:25 -0700

    Update wordle_helper.py

--- wordle_helper.py
+++ wordle_helper.py
@@ -1,8 +1,13 @@
+#/usr/bin/python3
 import json
 
-def load_words():
-    with open('path/to/words_dictionary.json') as word_file:
-        valid_words = json.load(word_file)
+def load_words(use_wordle_dictionary = False):
+    if use_wordle_dictionary:
+        with open('wordle_dictionary.json') as word_file:
+            valid_words = json.load(word_file)        
+    else:
+        with open('words_dictionary.json') as word_file:
+            valid_words = json.load(word_file)
 
     return valid_words
 
@@ -41,19 +46,30 @@ def get_potential_words(word_array, input, potential_letters, not_containing):
     return potentials
 
 if __name__ == '__main__':
-    # So far, I haven't seen Wordle use a word with repeating letters.
-    # In any case, let this be a variable in case that changes in the future.
-    INCLUDE_WORDS_WITH_REPEATING_LETTERS = False
+    INCLUDE_WORDS_WITH_REPEATING_LETTERS = True
+    USE_WORDLE_DICTIONARY = False
+    SHOW_HELP = False
 
     solved = False
     was_solved = ""
 
     print("Wordle!!")
 
-    english_words = load_words()
+    
+
+    if SHOW_HELP:
+        print("First try ETNAS")
+        print("If none of those letters match, try OCULI to check the three remaining vowels")
+
+    english_words = load_words(USE_WORDLE_DICTIONARY)
     potential_words = get_five_letter_words(english_words, INCLUDE_WORDS_WITH_REPEATING_LETTERS)
     words_without_aeiou = words_without_main_vowels(potential_words)
     possible_words_after_two_rounds_of_wordle = words_not_containing_letters(words_without_aeiou, "tnscl")
+
+    if SHOW_HELP:
+        print("If, somehow, none of those match as well, here are the possible remaining words:")
+        for word in possible_words_after_two_rounds_of_wordle:
+            print(word)
 
     while not solved:
         init_input = input("Enter a word for potential matches. Use \"_\" to signify GRAY or ORANGE letters: ")
@@ -62,7 +78,7 @@ if __name__ == '__main__':
 
         potential_words = get_potential_words(potential_words, init_input.lower(), potential_letters.lower(), not_containing.lower())
 
-        potential_word_string = ", ".join(potential_words)
+        potential_word_string = ", ".join(sorted(potential_words))
         print(f"Potential Words: {potential_word_string}")
 
         was_solved = input("Did you solve it? (yes/y/no/n): ")
